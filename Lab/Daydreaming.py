2c7b6b30db345c2849914119d1b61c50f9dd5b21
Nthan008 <114546358+Nthan008@users.noreply.github.com>
2022-10-19 18:27:33 +0700

    Thank You

--- Lab/Daydreaming.py
+++ Lab/Daydreaming.py
@@ -6,12 +6,12 @@ def game_with_cells(m, n):
             num = m*n//4 + m%n//2
         else :
             num = (m//2*2)*(n//2*2)/4 + m//2+n//2 + m*n%2
-    if m>1 or n>1:  
+    elif m>1 or n>1:  
         if (m%2 == 0 and n<2) or (n%2 == 0 and m<2):
             num = m*n//2
         elif m%2 != 0 and n%2 != 0:
             num = m*n//2 + m%n
     else:
         num = 1
-    return num
-print(game_with_cells(1,5))
+    return int(num)
+print(game_with_cells(9,9))
